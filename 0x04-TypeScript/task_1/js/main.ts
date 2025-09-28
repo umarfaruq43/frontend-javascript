ba5d8b2cdb0c4fda16d7aaffd199e3ffc9c6f74e
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-28 14:38:45 +0100

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,12 @@ interface Teacher {
     location: string;
     [key: string]: any;
 }
+
+const teacher3: Teacher = {
+    firstName: "John",
+    fullTimeEmployee: false,
+    lastName: "Doe",
+    location: "London",
+    contract: false,
+};
+console.log(teacher3);
